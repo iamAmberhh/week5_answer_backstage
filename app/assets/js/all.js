8cac797442f35189d973d55d12e09d1c20970328
Amber <melody19941231@gmail.com>
2022-08-26 18:18:12 +0800

    依助教建議修改

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,7 +1,7 @@
 $(function() {
-  if(location.pathname == '/index.html'){
+  if(window.location.pathname == '/week5_answer_backstage/index.html'){
       $('#assignment').addClass('list-active');
-  }else if(location.pathname == '/admin.html'){
+  }else if(location.pathname == '/week5_answer_backstage/admin.html'){
       $('#admin').addClass('list-active');
   }
 });
